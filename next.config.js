4bb515d96d6487745e89effb0d5337fe53d98f6d
rhys <rhysj6@gmail.com>
2023-04-08 18:45:40 +0100

    Changed env to include password hopefully

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   publicRuntimeConfig: {
     environment: process.env.ENVIRONMENT,
-    mysql_password: process.env.MYSQL_PASSWORD,
     mysql_server: process.env.MYSQL_SERVER,
     mysql_user: process.env.MYSQL_USER,
+    mysql_password: process.env.MYSQL_PASSWORD,
     mysql_database: process.env.MYSQL_DATABASE
   }
 }
