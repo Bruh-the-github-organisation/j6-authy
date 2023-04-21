8eb907575a0c1b6b6bd5f77b9c058ec102cf0fee
rhys <rhysj6@gmail.com>
2023-04-21 23:21:16 +0100

    Added redis utils

---
+++ src/utils/redis.js
@@ -0,0 +1,37 @@
+import Redis, { RedisOptions } from 'ioredis';
+
+
+const config = {
+    host: process.env.REDIS_HOST,
+    port: 6379,
+  }
+
+export function createRedisClient() {
+    try{
+        const options = {
+            ...config,
+            showFriendlyErrorStack: true,
+            enableAutoPipelining: true,
+            maxRetriesPerRequest: 0,
+            retryStrategy: (times) => {
+                if (times > 3) {
+                throw new Error(`[Redis] Could not connect after ${times} attempts`);
+                }
+    
+                return Math.min(times * 200, 1000);
+            },
+        }
+    
+        const redis = new Redis(options);
+        redis.on('error', (err) => {
+            console.error(`[Redis] Error: ${err.message}`);
+        });
+    }
+    catch(err){
+        console.error(`[Redis] Error: ${err.message}`);
+    }
+}
+
+const redis = createRedisClient();
+
+export { redis };
