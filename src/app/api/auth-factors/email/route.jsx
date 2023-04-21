1be654b827b7b2728e423256948652e48eed8b0e
rhys <rhysj6@gmail.com>
2023-04-21 23:41:24 +0100

    Added basic email stuff for testing redis

---
+++ src/app/api/auth-factors/email/route.jsx
@@ -0,0 +1,22 @@
+import {redis} from '@/utils/redis';
+
+export async function POST(request){
+    const body = await request.json()
+    const {username, password, sender} = body;
+    
+    // Save the email settings
+    await redis.set('email:username', username);
+    await redis.set('email:password', password);
+    await redis.set('email:sender', sender);
+}
+
+export async function GET(request){
+    const username = await redis.get('email:username');
+    const sender = await redis.get('email:sender');
+    return {
+        body: JSON.stringify({username, sender}),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    }
+}
